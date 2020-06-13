f6e621a6aaac08e9ebae2b8d8051170cdd42a8d0
Oleg Krasnov <OKrasnov@yotateam.ru>
2020-06-14 00:36:08 +0300

    initial project

---
+++ First Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First Set
+//
+//  Created by Краснов Олег on 14.06.2020.
+//  Copyright © 2020 Краснов Олег. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
